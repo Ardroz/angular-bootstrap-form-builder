e30a03841cdb123298c5b477f9e9cee261fca678
Enrique López Callejas <enrique199413@gmail.com>
2014-07-18 13:22:56 -0500

    :) checkboxes and radio inputs is done, :/ verify management of order in forms inner inputs

--- src/angular-bootstrap-form-builder.js
+++ src/angular-bootstrap-form-builder.js
@@ -56,12 +56,55 @@ function formBuilder ( templateCache ) {
                 placeholder: 'Pista'
               });
             break;
+          case 'append_text_input':
+            scope.form.inputs.push(
+              {
+                type: 'append_text_input',
+                input_type: 'text',
+                label: 'Texto postfijo',
+                append: 'M.N.',
+                placeholder: 'Pista'
+              });
+            break;
           case 'inline_radios':
             scope.form.inputs.push(
               {
                 type: 'inline_radios',
                 label: 'Radios en línea',
                 radios: []
+              });
+            break;
+          case 'list_radios':
+            scope.form.inputs.push(
+              {
+                type: 'list_radios',
+                label: 'Radios en lista',
+                radios: []
+              });
+            break;
+          case 'inline_checkboxes':
+            scope.form.inputs.push(
+              {
+                type: 'inline_checkboxes',
+                label: 'Checks en línea',
+                checkboxes: []
+              });
+            break;
+          case 'list_checkboxes':
+            scope.form.inputs.push(
+              {
+                type: 'list_checkboxes',
+                label: 'Checks en lista',
+                checkboxes: []
+              });
+            break;
+          case 'insert_button':
+            scope.form.inputs.push(
+              {
+                type: 'button',
+                value: 'input_value_button',
+                label: 'input_label_button' ? 'input_label_button' : 'btn btn-default',
+                size: 'input_size_button' ? 'input_size_button' : 'btn btn-default',
               });
             break;
         }
@@ -108,10 +151,15 @@ function formElement ( http, templateCache, compile, parse ) {
         if ( (/radio/g).test( this.input.type ) ) {
           this.input.radios.push({
             label: 'Radio ' + ( this.input.radios.length + 1 ),
-            value: this.input.radios.length + 1
+            value: this.input.radios.length + 1,
+            name : this.input.radios.name//check for the name is dinamyc or static
           });
         } else {
-
+          this.input.checkboxes.push({
+            label: 'Checkbox ' + ( this.input.checkboxes.length + 1 ),
+            value: this.input.checkboxes.length + 1,
+            name : this.input.checkboxes.name//check for the name is dinamyc or static
+          });
         }
       };
 
@@ -119,7 +167,7 @@ function formElement ( http, templateCache, compile, parse ) {
         if ( (/radio/g).test( this.$parent.input.type ) ) {
           this.$parent.input.radios.splice( this.$index, 1 );
         } else {
-
+          this.$parent.input.checkboxe.splice( this.$index, 1 );
         }
       };
     }
